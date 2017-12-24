81a7ce57c1908d84ce923f1a9f576f59b30f95a7
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-12-24 11:54:57 -0500

    Initial Commit

---
+++ Tom-JDBC/src/com/companyName/jdbc/DBCOnfi.java
@@ -0,0 +1,24 @@
+package com.companyName.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBCOnfi {
+// PRIVATE CONSTANTS =======
+private static final String USERNAME = "COMP228_F18_126";
+private static final String PASSWORD = "password";
+private static final String CONN_STRING = "jdbc:oracle:thin:@oracle1.centennialcollege.ca:1521:SQLD";
+// UTILITY METHODS THAT CONNECTS TO SQL SERVER===================
+public static Connection getConnection() throws SQLException {
+	return DriverManager.getConnection(CONN_STRING,USERNAME,PASSWORD);
+}
+// UTILITY METHOD THAT DISPLAYS OUR ERRORS IN MORE DETAILS===========
+public static void displayException(SQLException exception) {
+	System.err.println("Error Message: " + exception.getMessage());
+	System.err.println("Error COde: " + exception.getErrorCode());
+	System.err.println("SQL State: " + exception.getSQLState());
+}
+
+
+}
